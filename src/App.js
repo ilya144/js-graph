8cd0df4afbb712c9a2bc86acfb6a8e988bff3e41
ilya144 <ils12rus@gmail.com>
2019-12-13 14:16:30 +0300

    first setup of npm and webpack

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = props => {
+
+  return(
+    <p>Initial</p>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const isEnvProduction = false;
+
+module.exports = {
+  mode: 'none', //'production'
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js")
+  },
+  output: {
+    publicPath: './public',
+    path: path.resolve(__dirname, './build/'),
+    filename: 'static/js/[name].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'static/css/[name].css',
+    }),
+    new HtmlWebpackPlugin(
+      Object.assign(
+        {},
+        {
+          inject: true,
+          template: "./public/index.html",
+          // chunks: ["popup"],
+          filename: "popup.html"
+        },
+        isEnvProduction
+          ? {
+              minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+              },
+            }
+          : undefined
+      )
+    )
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+        },
+        'css-loader',]
+      },
+      {
+        test: /.(jpg|jpeg|png|svg)$/,
+        use: ['file-loader'],
+      },
+    ]
+  }
+};
+  

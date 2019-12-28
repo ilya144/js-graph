93894b8549cc221851b5f0985e42295c66694a05
ilya144 <ils12rus@gmail.com>
2019-12-28 12:57:17 +0300

    add react entry point

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
-import React from 'react';
-import {
-  Box,
-  Typography,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
-import ContactSupportIcon from '@material-ui/icons/ContactSupport';
-import GpsFixedIcon from '@material-ui/icons/GpsFixed';
-import HeightIcon from '@material-ui/icons/Height';
+import React, { useState } from "react";
+import { Container, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useClasses = makeStyles(theme => ({
-  icon: {
-    color: "white"
-  },
-  topBar: {
+import SideBar from "./components/sideBar";
+import SlideMenu from "./components/slideMenu";
+import GraphPage from "./components/graphPage";
+import Header from "./components/header";
+import Minimap from "./components/minimap";
+
+const useClasses = makeStyles(() => ({
+  app: {
     width: "100%",
-    backgroundColor: "#2b3151"
+    maxWidth: "100%",
+    height: "100vh",
+    maxHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    padding: "0",
+    margin: "0",
+
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
+    textAlign: "center"
+  },
+  content: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "row"
   }
 }));
 
-const App = props => {
+const App = () => {
   const classes = useClasses();
+  const [zoom, setZoom] = useState(1);
+  const [useCallback, setCallback] = useState(null);
 
-  return(
-    <Box 
-      className={classes.topBar}
-      display="flex"
-      flexDirection="row"
-    >
-      <MenuRoundedIcon className={classes.icon} />
-      <ContactSupportIcon className={classes.icon} />
+  return (
+    <Container className={classes.app}>
+      <Header zoom={zoom} useCallback={useCallback} />
 
-    </Box>
-  )
-}
+      <Box className={classes.content}>
+        <SlideMenu useCallback={useCallback} />
+        <GraphPage
+          setZoom={setZoom}
+          useCallback={useCallback}
+          setCallback={setCallback}
+        />
+        <SideBar />
+        <Minimap zoom={zoom} setZoom={setZoom} />
+      </Box>
+    </Container>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-)
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);

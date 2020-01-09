090e3fa83081e128bef72ee66c79b864e9217a99
ilya144 <ils12rus@gmail.com>
2020-01-09 09:05:23 +0300

    add methods for work with nodes

--- src/d3utils/graph.js
+++ src/d3utils/graph.js
@@ -1,6 +1,7 @@
 /* global window */
 import * as d3 from "d3";
-import { Node, NodeStore, GraphRender } from ".";
+import { Node, NodeStore, Edge, EdgeStore, GraphRender } from ".";
+import { runInThisContext } from "vm";
 
 class Graph extends GraphRender {
   constructor(entryRef, props, data) {
@@ -10,14 +11,13 @@ class Graph extends GraphRender {
     const main_svg = this.create_svg(root, entryRef);
     const outer = main_svg.append("g");
     const entry = outer.append("g");
-
     const zoom_obj = this.create_zoom([0.8, 2.5], [6400, 4096], () => {
       entry.attr("transform", d3.event.transform);
       props.setZoom(d3.event.transform.k);
     });
     window.onresize = this.create_resize_callback(main_svg, entryRef);
 
-    this.main(entry, data);
+    this.main(entry, data, props);
 
     outer.call(zoom_obj);
     props.setCallback({
@@ -40,15 +40,29 @@ class Graph extends GraphRender {
     });
   }
 
-  main(entry, data) {
-    // eslint-disable-next-line no-console
-    console.log(new NodeStore(data.nodes.map(node => new Node(node))));
-    this.draw_background(entry, Array(6));
+  main(entry, data, props) {
+    this.draw_background(entry, Array(6), props.isVertical);
+
+    this.nodes = new NodeStore(data.nodes.map(node => new Node(node)));
+    this.edges = new EdgeStore(data.edges.map(edge => new Edge(edge)));
+
+    console.log(this.nodes, this.edges);
+
+    this.nodes = new NodeStore(this.create_duplicates());
+    this.nodes = new NodeStore(this.add_parents());
+    this.fix_levels();
+    this.set_lvl_indexes();
+
+    this.draw_by_lvl_index(entry, this.nodes.getAll());
+    this.draw_edges_old(entry, this.nodes.getAll());
+
+    console.log(this.nodes);
   }
 
   create_svg(root, entryRef) {
     return root
       .append("svg")
+      .attr("id", "entry")
       .attr("xmlns", "http://www.w3.org/2000/svg")
       .style("position", "absolute")
       .attr("width", window.innerWidth - entryRef.offsetLeft)
@@ -70,6 +84,112 @@ class Graph extends GraphRender {
       .translateExtent([[0, 0], translateExtentMax])
       .on("zoom", callback);
   }
+
+  create_duplicates() {
+    if (this.nodes === undefined || this.edges === undefined)
+      throw Error("define stores first");
+
+    const nodes = this.nodes.getAll().flatMap(node => {
+      const edges_in = node.edges_in
+        .replace(/'/g, "")
+        .replace("[", "")
+        .replace("]", "")
+        .split(",");
+
+      if (edges_in.length > 1) {
+        return edges_in.map((edge, index) => {
+          const new_node = { ...node };
+          const isDuplicate = !!index;
+
+          new_node.edges_in = edge;
+          if (isDuplicate) {
+            new_node.edges_out = "";
+            new_node.leaf = false;
+          }
+          return new Node(new_node, isDuplicate);
+        });
+      }
+      return node;
+    });
+
+    return nodes;
+  }
+
+  add_parents() {
+    if (this.nodes === undefined || this.edges === undefined)
+      throw Error("define stores first");
+
+    const nodes = this.nodes.getAll().map(node => {
+      if (node.edges_in !== "") {
+        node.parent = this.nodes.getNode(this.edges.getEdge(node.edges_in).sid);
+      }
+      return node;
+    });
+
+    return nodes;
+  }
+
+  // Attention: this func has side effects - she change nodes in this.store
+  fix_levels() {
+    if (this.nodes === undefined || this.edges === undefined)
+      throw Error("define stores first");
+
+    const nodes = this.nodes.getAll();
+
+    // fix root level - just 0 + 1
+    const root = nodes.filter(node => node.level === 0)[0];
+    root.lvl = root.level + 1;
+
+    // fix dupes
+    nodes
+      .filter(node => node.isDuplicate)
+      .map(node => {
+        const parent = this.nodes.getNode(node.parent);
+        if (parent && node.lvl - parent.lvl > 1) {
+          node.lvl = parent.lvl + 1;
+        }
+        return node;
+      });
+
+    // fix technical nodes of parent-leaf
+    nodes
+      .filter(node => node.status === 2)
+      .map(node => {
+        const edge = this.edges.getEdgeByChild(node.pk);
+        const parent = this.nodes.getNode(edge && edge.sid);
+        if (parent && parent.leaf) {
+          node.lvl = parent.lvl;
+        }
+        return node;
+      });
+    return nodes;
+  }
+
+  set_lvl_indexes() {
+    if (this.nodes === undefined || this.edges === undefined)
+      throw Error("define stores first");
+
+    const nodes = this.nodes.getAll();
+    const nodesByLvls = [];
+    nodes.map(node => {
+      if (nodesByLvls[node.lvl] === undefined) {
+        nodesByLvls[node.lvl] = [];
+      }
+      nodesByLvls[node.lvl].push(node);
+      return node;
+    });
+
+    nodesByLvls.map(array =>
+      array.map((node, index) => {
+        node.lvlIndex = index;
+        return node;
+      })
+    );
+  }
+
+  sortByLvlIndex() {}
+
+  define_coords() {}
 }
 
 export default Graph;
